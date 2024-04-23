566c94a32c69fe902aa1efe2334346a4e258e150
Amariah <amariah.abish@gmail.com>
2024-04-23 10:27:49 +0300

    full test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Define NavBar component
 function NavBar() {
   return (
     <nav>
@@ -8,6 +9,7 @@ function NavBar() {
   );
 }
 
+// Define Home component
 function Home() {
   return (
     <div id="home">
@@ -16,15 +18,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+// Define About component
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
-
+// Define App component and include NavBar, Home, and About components
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
